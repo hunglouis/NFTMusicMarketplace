e0eedbebde7fd547db1c87aae6cb16154c7f73ec
hunglouis <83754071+hunglouis@users.noreply.github.com>
2026-04-16 10:24:03 +0700

    fix syntax

--- db.php
+++ db.php
@@ -7,13 +7,14 @@ $pass = "sb_publishable__5FJJ7E8LE8I1rhFXS2Z5A_SdiVSdYS";
 
 try {
     $conn = new PDO(
-        "DATABASE_URL=postgresql://postgres.hmvvjjiiaelcsfqgxbxv:[sb_publishable__5FJJ7E8LE8I1rhFXS2Z5A_SdiVSdYS]@aws-1-ap-northeast-1.pooler.supabase.com:5432/postgres
+        "DATABASE_URL=postgresql://postgres.hmvvjjiiaelcsfqgxbxv:[sb_publishable__5FJJ7E8LE8I1rhFXS2Z5A_SdiVSdYS]@aws-1-ap-northeast-1.pooler.supabase.com:5432/postgres"
+    );
 
     // bật lỗi để debug
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 } catch (PDOException $e) {
-    die("Kết nối thất bại: " . $e->getMessage());
+    die("Ket noi that bai:" . $e->getMessage());
 }
 ?>
 
