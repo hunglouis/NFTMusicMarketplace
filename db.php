e153c2ac921fc60bcb6e3085d99b6393c700a280
hunglouis <83754071+hunglouis@users.noreply.github.com>
2026-04-16 09:57:49 +0700

    fix supabase username

--- db.php
+++ db.php
@@ -1,16 +1,13 @@
 <?php
 $host = "aws-1-ap-northeast-1.pooler.supabase.com";
-$port = "6543";
+$port = "5432";
 $dbname = "postgres";
 $user = "postgres.hmvvjjiiaelcsfqgxbxv";
 $pass = "sb_publishable__5FJJ7E8LE8I1rhFXS2Z5A_SdiVSdYS";
 
 try {
     $conn = new PDO(
-        "pgsql:host=aws-0-ap-southeast-1.pooler.supabase.com;port=6543;dbname=postgres",
-    "postgres.hmvvjjiiaelcsfqgxbxv",
-    "pass"
-);
+        "DATABASE_URL=postgresql://postgres.hmvvjjiiaelcsfqgxbxv:[sb_publishable__5FJJ7E8LE8I1rhFXS2Z5A_SdiVSdYS]@aws-1-ap-northeast-1.pooler.supabase.com:5432/postgres
 
     // bật lỗi để debug
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
